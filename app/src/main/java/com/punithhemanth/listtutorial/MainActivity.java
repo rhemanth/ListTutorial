05ccd2901b2dc112e2798788ab1a7301e1c7a0e8
hkumar <hkumar@adobe.com>
2015-07-25 16:12:22 +0530

    Adding a contextual action menu

--- app/src/main/java/com/punithhemanth/listtutorial/MainActivity.java
+++ app/src/main/java/com/punithhemanth/listtutorial/MainActivity.java
@@ -2,17 +2,21 @@ package com.punithhemanth.listtutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActionMode.Callback{
 
     ListView listView;
     String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
@@ -21,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
             "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
             "Android", "iPhone", "WindowsMobile" };
     ArrayList<String> list;
+    protected Object mActionMode;
+    public int selectedItem = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +56,20 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(mActionMode!=null){
+                    return false;
+                }
+                selectedItem = i;
+                mActionMode = MainActivity.this.startActionMode(MainActivity.this);
+                view.setSelected(true);
+                return true;
+
+            }
+        });
+
     }
 
     @Override
@@ -72,5 +92,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+        MenuInflater inflater = actionMode.getMenuInflater();
+        inflater.inflate(R.menu.rowslection,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.menuitem1_show:
+                Toast.makeText(getApplicationContext(),Integer.toString(selectedItem),Toast.LENGTH_SHORT).show();
+                actionMode.finish();
+                return true;
+            default:
+                return  false;
+
+        }
+
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode actionMode) {
+        mActionMode = null;
+        selectedItem = -1;
+
     }
 }
